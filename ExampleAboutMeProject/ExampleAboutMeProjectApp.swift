d23cce6ee87dc1f85e83334aceb3c4c2b4dafeef
Marnie Reid <marnie@kodewithklossy.com>
2024-03-18 11:37:56 -0400

    Initial Commit

---
+++ ExampleAboutMeProject/ExampleAboutMeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAboutMeProjectApp.swift
+//  ExampleAboutMeProject
+//
+//  Created by scholar on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAboutMeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
